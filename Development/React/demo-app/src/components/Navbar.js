3eb5f1a021e7ad74b60929089fef3daa2faba069
d-yash <y.dhuliya.official@gmail.com>
2023-06-19 11:36:04 +0530

    19 June '23

--- Development/React/demo-app/src/components/Navbar.js
+++ Development/React/demo-app/src/components/Navbar.js
@@ -1,53 +1,28 @@
 import "../Style/Navbar.css";
-import Detail from "./Detail";
+import logo from "./logo.png";
 
 const Navbar = () => {
   return (
-    <div container>
-      <div className="tableContainer">
-        <table className="marksTable">
-          <caption className="tableCaption">Marks table</caption>
-          <thead>
-            <tr className="tableHeading">
-              <th>Name</th>
-              <th>Div</th>
-              <th>Marks</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Yash</th>
-              <th>B</th>
-              <th>99</th>
-            </tr>
-            <tr>
-              <th>Sonalben</th>
-              <th>E</th>
-              <th>86</th>
-            </tr>
-            <tr>
-              <th>Amitbhai</th>
-              <th>E</th>
-              <th>92</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="content">
-        {/* <Detail /> */}
-        <p>
-                dgnaksdhg asgas;hgasdhgasdjlaks d dga;sodgalsd
+    <nav className="bg-indigo-950 text-white">
+      <div className="mx-auto sm:px-6">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0 flex pl-12">
+            <img src={logo} alt="Logo" className="h-14 w-14" />
+            <p className="text-white mt-2 px-4 text-[28px] font-mono font-bold">
+              WhatsPhone
             </p>
-            <pre>
-                slgalsjfg s sf adfjgadl;
-                s slfgalk f
-                sdgnkas 
-                
-                [ dsfalskdjf ] = sdfasdhhh
-            </pre>
-            <h2>DONE</h2>
+          </div>
+          <div>
+            <ul className="flex text-left text-[24px]">
+              <li className="relative"><a href="#" className="mx-4 px-4 pb-1 border-b-2 hover:animate-bounce">Home</a></li>
+              <li className="relative"><a href="#" className="mx-4 px-4 pb-1 border-b-2 hover:animate-bounce">About</a></li>
+              <li className="relative"><a href="#" className="mx-4 px-4 pb-1 border-b-2 hover:animate-bounce">Catalogue</a></li>
+              <li className="relative"><a href="#" className="mx-4 px-4 pb-1 border-b-2 hover:animate-bounce">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
