cde2eee73ba6736cc6f16e9d8980fa09a7c57e8f
d-yash <y.dhuliya.official@gmail.com>
2023-06-22 02:16:14 +0530

    26 June '23

--- NextJS/portfolio/components/Navbar.jsx
+++ NextJS/portfolio/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4'>
       <div className='max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between'>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+        <motion.div initial={{y:-60, opacity: 0 }} animate={{y:0, opacity: 1 }} transition={{ duration: 0.8 }}>
           <Image src={logo} className='w-18 h-14' />
         </motion.div>
         <div className='hidden mdl:inline-flex items-center gap-9'>
